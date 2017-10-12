408683287d787d3fa98d18f92b0271eff29ab4b4
Sanjeet Tripathy <sanjeet.724@gmail.com>
2017-10-12 08:19:33 -0700

    added back the project file

--- Interviews/src/dialPad/Trial2.java
+++ Interviews/src/dialPad/Trial2.java
@@ -2,7 +2,9 @@ package dialPad;
 
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Trial2 {
 	
@@ -14,9 +16,11 @@ public class Trial2 {
 	public static int count_1;
 	public static int count_2;
 	public static String [] cache = new String[2];
-	public static List<String []> cache2 = new ArrayList<String[]>();
+
 	
-	public static String input = "4938532894754";
+	//public static String input = "4938532894754";
+	
+	public static String input = "12345678";
 	
 	public static void main(String[] args) {
 		doubleDigitNums = input.length() % 7;
@@ -35,81 +39,17 @@ public class Trial2 {
 			}
 			luckyPairs.add(pair);
 		}
-	    // printPairs(luckyPairs);
+	    printPairs(luckyPairs);
 		
 		// get combos
-		//createCombinations(luckyPairs);
-		
-		createCombinations2(luckyPairs);
+		createCombinations(luckyPairs);
 		
 		printResult(result);
 	}
 	
 	
-	/**
-	 * Creates the combinations
-	 * @param pairs - the list of options for each position
-	 * @param a - num of single digit numbers
-	 * @param b - num of double digit numbers
-	 */
-	public static void createCombinations(List<String[]> pairs) {
-		// base case
-		System.out.println("List Size: " + pairs.size());
-		if (pairs.size() == 1) {
-			String [] currPair = pairs.get(0);
-			int numericVal_1 = Integer.parseInt(currPair[0]);
-			if (!result.contains(currPair[0]) && count_1 < singleDigitNums && numericVal_1 > 0 ) {
-				result.add(currPair[0]);
-				count_1++;
-			}
-			System.out.println("Base Case");
-			return;
-		} 
-        
-		for (int i = 0; i < pairs.size(); i++) {
-			String [] currPair = pairs.get(i);
-			System.out.println("For Current Pair: " + currPair[0] + "," + currPair[1]);
-			System.out.println("Current Cache: " + cache[0] + "," + cache[1]);
-			int numericVal_1 = Integer.parseInt(currPair[0]);
-			int numericVal_2 = Integer.parseInt(currPair[1]);	
-			//handleSelection(currPair);
-			if (!result.contains(currPair[0]) && count_1 < singleDigitNums && numericVal_1 > 0 ) {
-				// select 1st one
-				System.out.println("  -->Selecting first one: " + currPair[0]);
-				result.add(currPair[0]);
-				count_1++;
-				cache[0] = currPair[0];
-				cache[1] = currPair[1];
-				pairs.remove(currPair);
-				System.out.println("  ----> recursing here: ");
-				createCombinations(pairs);
-			} else if (!result.contains(currPair[1]) && count_2 < doubleDigitNums && numericVal_2 < 60 && numericVal_2 > 0) {
-				// if 1st can't be selected, select 2nd one
-				System.out.println("  -->Selecting second one: " + currPair[1]);
-				result.add(currPair[1]);
-				count_2++;
-			    pairs.remove(currPair);
-			    System.out.println("  ----> recursing here2: ");
-			    createCombinations(pairs);
-			} else {
-				// 1st one cannot be selected because its either 0 or we have reached limit
-				// 2nd of next pair cannot be selected beacause it already exists or is greater than 70
-				// so select 2nd of 1st pair
-				 System.out.println("  -->Selecting from cache: " + cache[1]);
-				 result.remove(cache[0]);
-				 count_1--;
-				 int numericVal = Integer.parseInt(cache[1]);
-				 if (!result.contains(cache[1]) && count_2 < doubleDigitNums && numericVal < 60) {
-					 result.add(cache[1]);
-					 count_2++;
-					 //pairs.add(0, cache);
-					 //createCombinations(pairs);
-				 }
-			}
-		}
-	}
 	
-	public static void createCombinations2(List<String[]> pairs) {
+	public static void createCombinations(List<String[]> pairs) {
 		while (pairs.size() != 1) {
 			String [] currPair = pairs.get(0);
 			int num1 = Integer.parseInt(currPair[0]);
@@ -119,20 +59,26 @@ public class Trial2 {
 				System.out.println("  -->Selecting first one: " + currPair[0]);
 				result.add(currPair[0]);
 				count_1++;
-				cache[0] = currPair[0];
-				cache[1] = currPair[1];
-				cache2.add(currPair);
+				// only add to cache if eligible
+				if (!result.contains(currPair[1]) && count_2 < doubleDigitNums && num2 > 0 && num2 < 60) {
+					cache[0] = currPair[0];
+					cache[1] = currPair[1];
+				}
 				pairs.remove(currPair);
-				createCombinations2(pairs);
+				createCombinations(pairs);
 			} else if (!result.contains(currPair[1]) && count_2 < doubleDigitNums && num2 > 0 && num2 < 60) {
 				System.out.println("  -->Selecting 2nd one: " + currPair[1]);
 				result.add(currPair[1]);
 				count_2++;
-				cache2.add(currPair);
 				pairs.remove(currPair);
-				createCombinations2(pairs);
+				if (pairs.size() > 1) {
+					String [] nextPair = pairs.get(0);
+					pairs.remove(nextPair);
+				}
+				createCombinations(pairs);
 			} else {
 				// need to do course correction
+				System.out.println("  -->Discarding : " + cache[0]);
 				result.remove(cache[0]);
 				count_1--;
 				int numericVal = Integer.parseInt(cache[1]);
@@ -140,39 +86,32 @@ public class Trial2 {
 					System.out.println("  -->Selecting From Cache: " + cache[1]);
 					result.add(cache[1]);
 					count_2++;
-					System.out.println("  -->Cache2 size:  " + cache2.size());
 					//pairs.remove(currPair);
-					for (int i = cache2.size()-1; i >=0 ; i--) {
-						pairs.add(cache2.get(i));
-					}
-					createCombinations2(pairs);
+					createCombinations(pairs);
 				 }
+				else {
+					// invalid 
+				}
 			}
 		}
 		
 		if (pairs.size() == 1) {
 			String [] currPair = pairs.get(0);
 			int numericVal_1 = Integer.parseInt(currPair[0]);
+			int numericVal_2 = Integer.parseInt(currPair[1]);
+			//System.out.println("Base Case");
 			if (!result.contains(currPair[0]) && count_1 < singleDigitNums && numericVal_1 > 0 ) {
 				result.add(currPair[0]);
 				count_1++;
+			} else if (!result.contains(currPair[1]) && count_2 < doubleDigitNums && numericVal_2 > 0) {
+				result.add(currPair[1]);
+				count_2++;
 			}
-			//System.out.println("Base Case");
 			return;
 		} 
 	}
 	
-	public static void handleSelection(String [] currPair) {
-		int numericVal_1 = Integer.parseInt(currPair[0]);
-		int numericVal_2 = Integer.parseInt(currPair[1]);	
-		// check single digit
-		if (!result.contains(currPair[0]) && count_1 < singleDigitNums && numericVal_1 > 0) {
-			result.add(currPair[0]);
-			cache[0] = currPair[0];
-			cache[1] = currPair[1];
-			
-		}
-	}
+
 	
 	public static void printPairs(List<String[]> pairs) {
 		for (String [] pair : pairs) {
